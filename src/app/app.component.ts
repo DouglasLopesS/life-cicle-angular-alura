0e2189e1c979d272550318df289a3a5117d66643
unknown <douglas@sonax.net.br>
2023-09-11 18:39:23 -0300

    @Output(): comunicação de filho para pai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,8 @@ export class AppComponent implements OnInit {
     this.listaDeCompra = this.listaSerice.getListaDeCompra();
     console.log(this.listaDeCompra);
   }
+
+  editarItem(item: Item) {
+    console.log(item);
+  }
 }
